cd615ca85106c8f490eed9394935fabbea8fbcad
Дмитрий А. Мищуков <d.michukov@jusanmobile.kz>
2026-02-02 12:35:41 +0500

    добавили 0.0.0.0 интерфейсы

--- app.py
+++ app.py
@@ -124,5 +124,7 @@ def get_users():
 
     return jsonify(users)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render задает этот порт
+    app.run(host="0.0.0.0", port=port, debug=True)
